3d24931d4632f0b2cc6cddf4eced129f7c8683d5
dario-1 <renancitosalazar@gmail.com>
2023-10-03 18:24:34 -0500

    subventana

--- InterfazINEN.py
+++ InterfazINEN.py
@@ -11,6 +11,35 @@ from PyQt5.uic import loadUi
 from PyQt5.QtCore import pyqtSlot
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as figure
 import matplotlib.pyplot as plt
+class SubVentana(QtWidgets.QDialog):
+    def __init__(self,seleccion_lab):
+        super().__init__()
+        self.setWindowTitle("Subventana Personalizada")
+        loadUi('D:/ESPE/Practicas INEN/InterfazINEN/ventana.ui',self)
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setWindowOpacity(1)
+        self.frame_top2.mouseMoveEvent=self.mover_ventana
+        self.bt_close.clicked.connect(lambda: self.close())
+        self.var=seleccion_lab
+        self.labinfo.setText(self.var)
+        
+        
+        
+    
+    def mousePressEvent(self,event):
+        self.click_position=event.globalPos()
+
+    def mover_ventana(self,event):
+        if self.isMaximized()==False:
+            if event.buttons()==QtCore.Qt.LeftButton:
+                self.move(self.pos()+event.globalPos()-self.click_position)
+                self.click_position=event.globalPos()
+                event.accept()
+    
+    @QtCore.pyqtSlot(str)
+    def actualizar_info(self, mensaje):
+        self.label_info.setText(mensaje)
+        
 
 
 
@@ -20,6 +49,7 @@ class rooti(QMainWindow):
         loadUi('D:/ESPE/Practicas INEN/InterfazINEN/interfazinen2.ui',self)
         #oculatar boton normal
         self.bt_normal.hide()
+        self.frame_top.mouseMoveEvent=self.mover_ventana
          #eliminar la ventana del main
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
         self.setWindowOpacity(1)
@@ -32,16 +62,21 @@ class rooti(QMainWindow):
         self.bt_normal.clicked.connect(self.normal)
         self.bt_minimizar.clicked.connect(self.minimizar)
         self.bt_cerrar.clicked.connect(lambda: self.close())
+        #botones buscar, reporte y actualizar
+        self.bt_buscar.clicked.connect(self.buscar)
+        #self.bt_reporte.clicked.connect()
+        #self.bt_actualizar.connect()
         #botones de detalles
-        self.bt_tem1.clicked.connect(self.detalletem1)
-        self.bt_tem2.clicked.connect(self.detalletem2)
-        self.bt_humedad.clicked.connect(self.detallehume)
-        self.bt_presion.clicked.connect(self.detallepresi)
+        self.bt_tem1.clicked.connect(self.mostrar_subventana)
+        self.bt_tem2.clicked.connect(self.mostrar_subventana)
+        self.bt_humedad.clicked.connect(self.mostrar_subventana)
+        self.bt_presion.clicked.connect(self.mostrar_subventana)
         #condiciones iniciales del combobox seleccion_lab
         self.seleccion_lab.model().item(0).setEnabled(False)
         #====gráficas=====
         self.init_graph()  
     #======================FUNCIONES=========
+
     def minimizar(self):
         self.showMinimized()
     def normal(self):
@@ -54,15 +89,57 @@ class rooti(QMainWindow):
         self.bt_maximizar.hide()
     def mousePressEvent(self,event):
         self.click_position=event.globalPos()
-    #===========Fuenciones detalles====
-    def detalletem1(self):
-         QMessageBox.warning(self, 'Advertencia', 'No se seleccionó ningún archivo.')
+    #===========Funciones detalles====
+    def mostrar_subventana(self):
+        subventana = SubVentana(self.seleccion_lab.currentText())
+        subventana.exec_()
+    
+    #def detalletem1(self):
+         #QMessageBox.warning(self, 'Advertencia', 'No se seleccionó ningún archivo.')
     def detalletem2(self):
          QMessageBox.warning(self, 'Advertencia', 'No se seleccionó ningún archivo.')
     def detallehume(self):
          QMessageBox.warning(self, 'Advertencia', 'No se seleccionó ningún archivo.')
     def detallepresi(self):
          QMessageBox.warning(self, 'Advertencia', 'No se seleccionó ningún archivo.')
+    #====Funcion  buscar====
+    def buscar(self):
+        if self.seleccion_lab.currentText()=="Seleccioné una Opción":
+            self.nombre_lab.setText("No se ha seleccionado un lab ") 
+            QMessageBox.warning(self, 'Advertencia', 'No se seleccionó un laboratorio.')
+             
+        else: 
+            lab_seleccionado = self.seleccion_lab.currentText()
+            self.nombre_lab.setText("Laboratorio de "+lab_seleccionado)
+            #==variable de la fecha==aquí se captura de la base de datos la fecha:
+            fecha="03/10/2023"# cambiar el valor por la variable que se captura del SQL
+            self.fecha.setText(fecha)
+            #==variable de la hora==aquí se captura de la base de datos la hora:
+            hora="15:30"
+            self.hora.setText(hora)
+        #======Función actualizar y reporte
+        def report(self):
+            archivo=''#aquí se debería colocar el archivo del reporte
+        def actual(self):
+            refresh=''
+            
+     #=========mover ventana====
+    def mover_ventana(self,event):
+        if self.isMaximized()==False:
+            if event.buttons()==QtCore.Qt.LeftButton:
+                self.move(self.pos()+event.globalPos()-self.click_position)
+                self.click_position=event.globalPos()
+                event.accept()
+        if event.globalPos().y()<=10:
+            self.showMaximized()
+            self.bt_maximizar.hide()
+            self.bt_normal.show()
+        else:
+            self.showNormal()
+            self.bt_maximizar.show()
+            self.bt_normal.hide()
+        
+
      #====funcion de las gráficas=====
     def init_graph(self):
         self.grafica1 = Canvas_tempe1('temperatura')
